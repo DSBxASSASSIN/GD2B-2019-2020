edbde3f3b771cd469ade4ee8e8dd58b360dbacc0
thijs van slooten <41576972+DSBxASSASSIN@users.noreply.github.com>
2019-09-20 14:30:15 +0200

    hans heeft een pipi

---
+++ html/myPoint/main.js
@@ -0,0 +1,29 @@
+const ctx = document.querySelector("canvas").getContext("2d");
+
+ctx.canvas.width = document.documentElement.clientWidth;
+ctx.canvas.height = document.documentElement.clientHeight;
+
+
+let balls = [];
+
+function Update(){
+    requestAnimationFrame(Update);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    let ball = new Ball(new Vector2d(getRandom(ctx.canvas.width), getRandom(ctx.canvas.height)), 30, getRandom(255), getRandom(255), getRandom(255));
+    balls.push(ball);
+
+    for(i = 0; i < balls.length; i++){
+        balls[i].radius += 0.5;
+        balls[i].draw(ctx);
+
+        if(balls[i].radius > 80){
+            balls.splice(i,1);
+        }
+    }
+}
+
+function getRandom(max){
+    return Math.floor(Math.random() * max);
+}
+
+Update();
