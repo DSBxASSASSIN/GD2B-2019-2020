08670ef00970a1f420f57355e8d5a2a3c3de82b8
thijs van slooten <41576972+DSBxASSASSIN@users.noreply.github.com>
2019-10-01 20:18:05 +0200

    een commit

--- html/dot_line_dot/main.js
+++ html/dot_line_dot/main.js
@@ -3,23 +3,29 @@ const ctx = document.querySelector("canvas").getContext('2d');
 ctx.canvas.width = document.documentElement.clientWidth;
 ctx.canvas.height = document.documentElement.clientHeight;
 
-let a = new Ball(new Vector2d(300,200), 15, true);
-let b = new Ball(new Vector2d(500,300), 15, true);
+let a = new Ball(new Vector2d(getRandom(ctx.canvas.width), getRandom(ctx.canvas.height)), 15, true);
+let b = new Ball(new Vector2d(getRandom(ctx.canvas.width), getRandom(ctx.canvas.height)), 15, true);
 
 let l = new Liniar(1/3, 133);
 
 
-for (let x = 0; x < ctx.canvas.width; x+= 10) {
-    let a = new Ball(new Vector2d(x ,l.calcY(x)), 5);
-    a.draw(ctx);
-}
 
 function Update() {
     requestAnimationFrame(Update);
+    
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    l.defineLineByTwoPoints(a, b);
     a.draw(ctx);
     b.draw(ctx);
-    l.defineLineByTwoPoints(a, b);
+    
+
+    for (let x = 0; x < ctx.canvas.width; x+= 10) {
+        let a = new Ball(new Vector2d(x ,l.calcY(x)), 5);
+        a.draw(ctx);
+    }
 }
 
+function getRandom(max) {
+    return Math.floor(Math.random() * max);
+}
 Update()
---
+++ html/fil_dots/main.js
@@ -0,0 +1,51 @@
+const ctx = document.querySelector('canvas').getContext('2d');
+
+ctx.canvas.width = document.documentElement.clientWidth;
+ctx.canvas.height = document.documentElement.clientHeight;
+
+
+let dots = [];
+
+for (var i = 0; i <4; i++) {
+  addPoint();
+}
+
+function animate(){
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  
+  if(Math.random() < 0.01){
+     dots.splice(0,1)
+     addPoint();
+
+  }
+
+  ctx.fillStyle = "rgba(255,255,0,0.2)"
+  ctx.beginPath();
+  ctx.moveTo(dots[0].pos.dx,dots[0].pos.dy);
+  
+  for (var i = 0; i < dots.length; i++) {
+    ctx.lineWidth = 2;
+    ctx.lineTo(dots[i].pos.dx,dots[i].pos.dy);
+  }
+
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+  
+  for (var i = 0; i < dots.length; i++) {
+    dots[i].draw(ctx);
+
+  }
+}
+
+animate();
+
+function randomNumber(max){
+  return Math.random()*max;
+}
+
+function addPoint(){
+  let dot = new Ball(new Vector2d(randomNumber(ctx.canvas.width),randomNumber(ctx.canvas.height)),30,"yellow");
+  dots.push(dot);
+}
