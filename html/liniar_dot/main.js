03181a094e930510929b656545cf9052c7d79976
thijs van slooten <41576972+DSBxASSASSIN@users.noreply.github.com>
2019-10-01 13:55:22 +0200

    help mij gek

---
+++ html/liniar_dot/main.js
@@ -0,0 +1,11 @@
+const ctx = document.querySelector('canvas').getContext('2d');
+
+ctx.canvas.width = document.documentElement.clientWidth;
+ctx.canvas.height = document.documentElement.clientHeight;
+
+let f = new Liniar(1,10);
+
+for(let i = 0; i < ctx.canvas.width; i+=10){
+    let point = new Ball(new Vector2d(i,f.calcY(i)), 10);
+    point.draw(ctx);
+}
