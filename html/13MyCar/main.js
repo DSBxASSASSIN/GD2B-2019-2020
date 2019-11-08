82ac59ee94566a0f5f9aa2448b5c11ef05253dad
thijs van slooten <41576972+DSBxASSASSIN@users.noreply.github.com>
2019-11-08 14:53:05 +0100

    hans

--- html/13MyCar/main.js
+++ html/13MyCar/main.js
@@ -1,6 +1,6 @@
 let ctx = document.querySelector("canvas").getContext('2d');
 
-
+let angle = 0;
 
 let car = new Image();
 car.src = "images/car.png";
@@ -12,10 +12,14 @@ let wheelFront = new Image();
 wheelFront.src = "images/wheel.png";
 wheelFront.pos = new Vector2d(0, 0);
 
+let wheelBack = new Image();
+wheelBack.src = "images/wheel.png";
+wheelBack.pos = new Vector2d(0, 0);
+
 
 car.addEventListener('load', ()=>{
     
-    car.pos.dy = ctx.canvas.height - car.height;
+    car.pos.dy = document.documentElement.clientHeight - car.height;
     
     Update();
 })
@@ -29,11 +33,23 @@ function Update(){
 
     car.pos.add(car.vel);
     ctx.drawImage(car, car.pos.dx, car.pos.dy);
+
     wheelFront.pos.dx = car.pos.dx;
     wheelFront.pos.dy = car.pos.dy;
     wheelFront.pos.add(new Vector2d(673, 123));
 
+    wheelBack.pos.dx = car.pos.dx;
+    wheelBack.pos.dy = car.pos.dy;
+    wheelBack.pos.add(new Vector2d(135, 123));
+    
+    ctx.save();
+    ctx.translate();
+    ctx.rotate(angle);
     ctx.drawImage(wheelFront, wheelFront.pos.dx, wheelFront.pos.dy);
+    ctx.drawImage(wheelBack, wheelBack.pos.dx, wheelBack.pos.dy);
+    ctx.restore();
+
+    angle += 0.1;
     clamp()
 }
 
