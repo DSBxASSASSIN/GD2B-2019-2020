0766e705fc8e8624c3851df6ec0e7490a96b22b8
thijs van slooten <41576972+DSBxASSASSIN@users.noreply.github.com>
2020-01-14 14:25:48 +0100

    pls

--- html/15WayPoints/main.js
+++ html/15WayPoints/main.js
@@ -4,22 +4,35 @@ ctx.canvas.width = document.documentElement.clientWidth;
 ctx.canvas.height = document.documentElement.clientHeight;
 
 let waypoints = [];
-
-for (let i = 0; i < 5; i++) {
-    waypoints.push(new Ball(new Vector2d(random(ctx.canvas.width), random(ctx.canvas.height)), 15));
+let ammountOfWaypoints = 5;
+for (let i = 0; i < ammountOfWaypoints; i++) {
+    waypoints.push(new Ball(new Vector2d(random(ctx.canvas.width), random(ctx.canvas.height)), 20, 255, 0, 0));
 }
+
+let player = new DPoint(new Vector2d(waypoints[0].pos.dx, waypoints[0].pos.dy), 15, new Vector2d(0,0), new Vector2d(0,0), 255, 255, 0);
 
 function Update(){
     requestAnimationFrame(Update);
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    // drawing the balls and lines
+    for(let i = 0; i < waypoints.length; i++){
+        waypoints[i].draw(ctx);
+        if(waypoints[i + 1] == !null){
+            ctx.beginPath();
+            ctx.moveTo(waypoints[i].pos.dx, waypoints[i].pos.dy);
+            ctx.lineTo(waypoints[i+1].pos.dx, waypoints[i+1].pos.dy);
+            ctx.closePath();
+            ctx.stroke();
+        }else{
+            ctx.beginPath();
+            ctx.moveTo(waypoints[i].pos.dx, waypoints[i].pos.dy);
+            ctx.lineTo(waypoints[0].pos.dx, waypoints[0].pos.dy);
+            ctx.closePath();
+            ctx.stroke();
+        }
+    }
     
-
-    // console.log(waypoints);
-    // // drawing the balls and lines
-    // waypoints.forEach(point => {
-    //     console.log("hans");
-    //     point.draw(ctx);
-    // });
+    player.draw(ctx);
 }
 
 function random(max){
