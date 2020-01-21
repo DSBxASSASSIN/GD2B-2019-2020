98be6d690e329fd5361a312c4fabfeabed5a2454
thijs van slooten <41576972+DSBxASSASSIN@users.noreply.github.com>
2020-01-21 13:41:14 +0100

    help

--- html/15WayPoints/main.js
+++ html/15WayPoints/main.js
@@ -10,14 +10,13 @@ for (let i = 0; i < ammountOfWaypoints; i++) {
 }
 
 let currentWayPoint = 1;
-let target = waypoints[currentWayPoint];
-let targetdis = new Vector2d(0,0);
 let ball = new DPoint(new Vector2d(waypoints[0].pos.dx, waypoints[0].pos.dy), 15, new Vector2d(0,0), new Vector2d(0,0), 255, 255, 0);
 
 function Update(){
     requestAnimationFrame(Update);
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     
+    // drawing the balls and lines
     ctx.beginPath();
     ctx.moveTo(waypoints[0].pos.dx, waypoints[0].pos.dy);
     for(let i = 1; i < waypoints.length; i++){
@@ -26,22 +25,12 @@ function Update(){
     ctx.closePath();
     ctx.stroke();
     
-    // drawing the balls and lines
     for(let i = 0; i < waypoints.length; i++){
         waypoints[i].draw(ctx);
     }
     
     // balls controller
-    targetdis.differenceVector(target.pos, ball.pos);
-    if(targetdis.dx <= 0 || targetdis.dy <= 0 && currentWayPoint < waypoints.length){
-        target = waypoints[currentWayPoint + 1];
-    }else{
-        currentWayPoint = 0;
-        target = waypoints[currentWayPoint];
-    }
-    ball.vel.differenceVector(target.pos, ball.pos);
-    
-    ball.vel.scalarMul(0.04);
+    ball.vel.differenceVector(ball.pos, waypoints[currentWayPoint].pos);
     ball.draw(ctx);
     ball.update();
     ball.vel.draw(ctx, ball.pos);
