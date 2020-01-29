93058b43d58d2ede813a38bed7a3c23d03f6e605
thijs van slooten <41576972+DSBxASSASSIN@users.noreply.github.com>
2020-01-29 11:06:32 +0100

    opdracht 15 werkt

--- html/15WayPoints/main.js
+++ html/15WayPoints/main.js
@@ -9,8 +9,10 @@ for (let i = 0; i < ammountOfWaypoints; i++) {
     waypoints.push(new Ball(new Vector2d(random(ctx.canvas.width), random(ctx.canvas.height)), 20, false, 255, 0, 0));
 }
 
-let ball = new DPoint(new Vector2d(waypoints[0].pos.dx, waypoints[0].pos.dy), 15, new Vector2d(0,0), new Vector2d(0,0), 255, 255, 0);
-let currentWayPoint = 1;
+let ball = new DPoint(new Vector2d(waypoints[0].pos.dx, waypoints[0].pos.dy), 15, new Vector2d(0,0), new Vector2d(0,0), 255, 255, 0, 1, 1);
+let ball2 = new DPoint(new Vector2d(waypoints[1].pos.dx, waypoints[1].pos.dy), 15, new Vector2d(0,0), new Vector2d(0,0), 255, 255, 0, 1, 2);
+let ball3 = new DPoint(new Vector2d(waypoints[2].pos.dx, waypoints[2].pos.dy), 15, new Vector2d(0,0), new Vector2d(0,0), 255, 255, 0, 1, 3);
+let ball4 = new DPoint(new Vector2d(waypoints[3].pos.dx, waypoints[3].pos.dy), 15, new Vector2d(0,0), new Vector2d(0,0), 255, 255, 0, 1, 4);
 
 function Update(){
     requestAnimationFrame(Update);
@@ -29,19 +31,23 @@ function Update(){
         waypoints[i].draw(ctx);
     }
     
-    // balls controller
-    ball.vel.differenceVector(waypoints[currentWayPoint].pos, ball.pos);
-    ball.vel.scalarMul(0.01);
-    console.log(ball.vel.magnitude);
-    if(ball.vel.magnitude < 0.1){
-        currentWayPoint++;
-        if(currentWayPoint > waypoints.length -1){
-            currentWayPoint = 0;
-        }
-    }
+    
+    ball.followWaypoints(waypoints);
+    ball2.followWaypoints(waypoints);
+    ball3.followWaypoints(waypoints);
+    ball4.followWaypoints(waypoints);
     ball.draw(ctx);
+    ball2.draw(ctx);
+    ball3.draw(ctx);
+    ball4.draw(ctx);
     ball.update();
+    ball2.update();
+    ball3.update();
+    ball4.update();
     ball.vel.draw(ctx, ball.pos, 10);
+    ball2.vel.draw(ctx, ball2.pos, 10);
+    ball3.vel.draw(ctx, ball3.pos, 10);
+    ball4.vel.draw(ctx, ball4.pos, 10);
 }
 
 function random(max){
